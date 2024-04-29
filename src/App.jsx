ba52e6b059d220e42e3a2425efca9440acc0b491
abhisheksrivastavofficial <abhisheksrivastavofficial@gmail.com>
2024-04-29 12:12:51 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth.service"
+import { login, logout} from "./store/authSlice"
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1 className={`bg-orange-400 text-black outline-white flex justify-center`}>ReactBlogX</h1>
-    </>
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    //.catch(() => dispatch(logout()))
+    .finally(() => setLoading(false))
+  }, [])
+
+
+  return !loading && (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <h1>ReactBlogX</h1>
+        <Footer />
+      </header>
+    </div>
   )
 }
 
